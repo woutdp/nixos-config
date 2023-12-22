e4afa65993cc88aa687bd1b5f74534ffbcd5f696
Wout De Puysseleir <woutdp@gmail.com>
2023-12-21 18:02:01 -0800

    Run postgres server

--- hosts/framework/default.nix
+++ hosts/framework/default.nix
@@ -126,6 +126,20 @@
       '';
     };
 
+    postgresql = {
+      enable = true;
+      ensureDatabases = [ "mydatabase" ];
+      authentication = pkgs.lib.mkOverride 10 ''
+        #type database  DBuser     auth-method
+        local all       all        trust
+        local all       all        md5
+        local all       postgres   peer
+        host  all       postgres   localhost               trust
+        host  all       all        127.0.0.1/32            md5
+        host  all       all        ::1/128                 ident
+      '';
+    };
+
     fwupd.enable = true;
     blueman.enable = true;
     upower.enable = true;
