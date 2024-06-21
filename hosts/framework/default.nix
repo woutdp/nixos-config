48c1df228ab4824f227eb9263cc4e60e4d961698
Wout De Puysseleir <woutdp@gmail.com>
2024-06-21 21:24:11 +0300

    Set default browser

--- hosts/framework/default.nix
+++ hosts/framework/default.nix
@@ -227,6 +227,12 @@
     sessionVariables.NEOVIDE_MULTIGRID = "1";
   };
 
+  xdg.mime.defaultApplications = {
+    "x-scheme-handler/http" = "google-chrome.desktop";
+    "x-scheme-handler/https" = "google-chrome.desktop";
+    "text/html" = "google-chrome.desktop";
+  };
+
   programs = {
     hyprland = {
       enable = true;
